ae3cdeae42cd4572d9b892bd2b546e2862a36215
karim wydadi <don.karimmansour@gmail.com>
2022-03-06 13:41:56 +0000

    first

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 
 
-//const DB_URL = "mongodb://localhost:27017/test"
-const DB_URL = "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb"
+const DB_URL = "mongodb://localhost:27017/test"
+//const DB_URL = "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb"
 
 function DB(){
     return mongoose.connect(DB_URL, (err) => {
