6c09ed5fc9ed782b6b146c96c3f2ced135155b1e
karim wydadi <don.karimmansour@gmail.com>
2022-03-18 11:34:10 +0000

    nex

--- database/db.js
+++ database/db.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose")
+const app = require("express")()
 
-
-const DB_URL = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "mongodb://localhost:27017/test" : "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb" ;
+const DB_URL = (app.get("env") == "development") ? "mongodb://localhost:27017/test" : "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb" ;
 
 function DB(){ 
+    console.log("db start" , (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "mongodb://localhost:27017/test" : "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb")
+
     return mongoose.connect(DB_URL, (err) => {
             if (err) 
                 throw new Error("db error")
             
             console.log("db start")
 })
-
-}
+} 
  
 
-module.exports = DB
+module.exports = DB 
--- versions/v1.js
+++ versions/v1.js
@@ -16,12 +16,12 @@ app.use((req, res, next) => {
     res.status(404).json("Api not found") 
 })
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+if (app.get("env") == "development") {
     app.listen(process.env.PORT || 3001 , () => {
-        console.log("server start")
+        console.log("server start dev")
     })
 } else {
     app.listen(2000 , () => {
-        console.log("server start")
+        console.log("server start prod")
     })
 }
