c50de708b5f12fbd3d0add5e0944488156b40e1a
karim wydadi <don.karimmansour@gmail.com>
2022-03-20 21:18:22 +0000

    new

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const app = require("express")()
 
-const DB_URL = (app.get("env") == "development") ? "mongodb://localhost:27017/test" : "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb" ;
-//const DB_URL = "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb" ;
+//const DB_URL = (app.get("env") == "development") ? "mongodb://localhost:27017/test" : "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb" ;
+const DB_URL = "mongodb+srv://admin:admin@cluster0.3av2c.mongodb.net/workdb" ;
 
 function DB(){ 
 
