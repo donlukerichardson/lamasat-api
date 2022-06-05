d1f5ab08982787608bd5cb29f2a7c3f6b950c58a
karim wydadi <don.karimmansour@gmail.com>
2022-06-05 12:56:51 +0100

    nw

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
-const passport = require("passport")
 const DB = require("./database/db")
 const cors = require("cors");
 const { Host } = require("./common/apiEndpoints")
@@ -17,38 +16,15 @@ if (app.get("env") == "development") {
     app.use(morgan("dev"))
 }
 
-const corsOptions = {
-    origin: [ "compareprime.com" , "www.compareprime.com" ,"dashboard.compareprime.com" ,"milo-jbilu.xyz"]
-}  
+// const corsOptions = {
+//     origin: [ "compareprime.com" , "www.compareprime.com" ,"dashboard.compareprime.com" ,"milo-jbilu.xyz"]
+// }  
  
-
-//origin : `${Host.FRONTEND}`
-
-//const  path  = require("path") 
-//const  fs  = require("fs")
-
-
-// app.get("/images/*" , (req , res) => {
-//     fs.readFile(`public/images/b.png`, function(err, data) {
-//         if (err) res.status(401).json({"error": "yes", "message": "There was an error fetching your image"}) ;
-//         else {
-//           res.writeHead(200, {'Content-Type': 'image/jpeg'});
-//           res.end(data); 
-//         }
-//       });
-// });
-
-// console.log(path.join(__dirname ,'public' , 'images'));
-
 
 //decoded data
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use( cors() );
 
-
-//passport initialize
-require("./config/passport")
-app.use(passport.initialize());
 
 module.exports = {app }
